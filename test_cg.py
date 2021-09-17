d24334764fa175a56db3d597fd4a71a53b843b72
Alex <sumy.ua@gmail.com>
2021-09-17 12:02:07 +0300

    fields

--- test_cg.py
+++ test_cg.py
@@ -3,6 +3,12 @@ import requests
 
 coingecko = "https://www.coingecko.com/en"
 r = requests.get(coingecko)
-table = pd.read_html(r.text)
+fields = ["Coin", "Price", "Mkt Cap", "1h","24h","7d"]
+table = pd.read_html(r.text)[0][fields]
+
+#left symbol only
+table["Coin"] = table ["Coin"].apply(lambda x: x.split("  ")[2])
+for s in fields[1], fields[2]:
+	table[s] = table [s].apply(lambda x: x.replace(",","").replace("$",""))
 
 print(table)
